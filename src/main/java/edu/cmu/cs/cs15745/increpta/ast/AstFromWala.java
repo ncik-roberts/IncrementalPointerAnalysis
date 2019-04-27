c396c442893a97a3d713a0cca97cf7f873627298
ncik-roberts <nroberts@andrew.cmu.edu>
2019-04-26 21:04:13 -0400

    Add WALA as a dependency.

---
+++ src/main/java/edu/cmu/cs/cs15745/increpta/ast/AstFromWala.java
@@ -0,0 +1,13 @@
+package edu.cmu.cs.cs15745.increpta.ast;
+
+import com.ibm.wala.ipa.callgraph.CallGraph;
+
+/**
+ * Utility class from converting a WALA representation of a Java program
+ * into the simplified Ast for our pointer analysis.
+ */
+public final class AstFromWala {
+	public static final Ast convert(CallGraph graph) {
+		return null;
+	}
+}
